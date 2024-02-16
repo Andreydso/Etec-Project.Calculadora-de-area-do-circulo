7111308857cd538cf4a567c31d9845a7393ef592
Andrey Oliveira <58522443+andreygamer3678@users.noreply.github.com>
2024-02-15 22:00:19 -0300

    Add files via upload

---
+++ teste001/Program.cs
@@ -0,0 +1,21 @@
+﻿using System; 
+
+class URI {
+
+    static void Main(string[] args) { 
+        Console.WriteLine("-------------------------------");
+        Console.WriteLine("CALCULADORA DA ÁREA DO CÍRCULO!");
+        Console.WriteLine("      Feito por Andrey");
+        Console.WriteLine("-------------------------------");
+        Console.WriteLine("");
+        Console.Write("Digite o raio do círculo: ");
+        Double Raio = Convert.ToDouble(Console.ReadLine());
+        Double Pi = 3.14159;
+
+        Double RaioII = Raio * Raio;
+        Double Result = Pi * RaioII;
+
+        Console.WriteLine("Área do círculo: " + Math.Round(Result, 4));
+    }
+    
+}
